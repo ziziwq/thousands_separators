0a489ffec589d0556cdf1f5a952ae2a2bbd90190
ziziwq <ziziwq@users.noreply.github.com>
2016-05-26 17:22:55 +0800

    Update separators.js

--- src/separators.js
+++ src/separators.js
@@ -1,6 +1,32 @@
 'use strict';
 
-function thousands_separators(num) {
+function thousands_separators(num) {var num = (num || 0).toString(), re = /d{3}$/, result = '';
+
+    while ( re.test(num) ) {
+
+        result = RegExp.lastMatch + result;
+
+        if (num !== RegExp.lastMatch) {
+
+            result = ',' + result;
+
+            num = RegExp.leftContext;
+
+        } else {
+
+            num = '';
+
+            break;
+
+        }
+
+    }
+
+    if (num) { result = num + result; }
+
+    return result;
+
+}
   
 }
 
